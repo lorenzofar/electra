aca10c2bf9ddb673df86e7e9a5c9e8640efb53c3
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-21 18:10:13 +0100

    Add class to manage connected users
    The class relies on the cache manager to get the list of connected users

---
+++ src/swarmManager.ts
@@ -0,0 +1,30 @@
+import User from "./models/user";
+import CacheManager from "./cacheManager";
+
+class SwarmManager {
+    private static _instance: SwarmManager = null;
+
+    public static get Instance(): SwarmManager {
+        if (this._instance) this._instance = new SwarmManager();
+        return this._instance;
+    }
+
+    /* ===== PROVIDERS ===== */
+    public get activeUsers() {
+        let keys = CacheManager.keys; // Get user ids
+        return keys.map(k => CacheManager.getUser(k)); // Get user objects
+    }
+
+    /* ===== STACK MANAGEMENT ===== */
+    public addUser(user: User): boolean {
+        //TODO: Notify cacheManager
+        let result = CacheManager.addEntry(user);
+        return result;
+    }
+
+    public removeUser(user: User): boolean {
+        //TODO: Notify cacheManager
+        let result = CacheManager.removeEntry(user.id);
+        return result;
+    }
+}
