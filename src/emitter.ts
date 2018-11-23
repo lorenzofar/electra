4c9fb33e2044126d74c7cf22ac44bb3872c93f36
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-23 18:53:07 +0100

    Fixed logic of incoming connections handling

--- src/emitter.ts
+++ src/emitter.ts
@@ -1,6 +1,10 @@
 import * as socket_io from "socket.io";
 import { Server } from "http";
 
+import * as messages from "./models/messages";
+
+import Validator from "./validator";
+
 class Emitter {
     private static io: socket_io.Server = null;
 
@@ -15,14 +19,25 @@ class Emitter {
     }
 
     private static incomingConnectionHandler(socket: socket_io.Socket) {
-        console.log("client connected");
-        socket.on("disconnect", this.disconnectionHandler);
+        let clientData: messages.socketHandshake = socket.handshake.query;
+
+        // Check if the client that is trying to connect can do it
+        // If not, send an error message and close the connection
+        if (!Validator.validateHandshake(clientData))
+            socket.send("unauthorized").disconnect();
+
+        // The client passed the test, it can connect
+        // Add it to the swarm 
+        console.log(`${clientData.email} is connnected`);
+
+        // Add disconnection listener to detect when a client goes offline
+        socket.on("disconnect", this.disconnectionHandler.bind(this, socket, clientData.email));
         //TODO: Save client data and bind disconnection handler to him
-        
+
     }
 
-    private static disconnectionHandler(socket: socket_io.Socket){
-        console.log("client disconnected");
+    private static disconnectionHandler(socket: socket_io.Socket, email: string) {
+        console.log(`${email} disconnected`);
     }
 }
 
