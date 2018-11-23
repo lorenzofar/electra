e23c24277be38d669bfa1887230f163134cc03e3
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-23 18:51:19 +0100

    Started adding interfaces of exchanged messages

---
+++ src/models/messages.ts
@@ -0,0 +1,13 @@
+/**
+ * This interface provides a definition of the messages
+ * exchanged by the server and the device
+ */
+
+ /**
+  * This interface defines the format of messages exchanged during socket handshake
+  * It extends the base class of SocketIO.Handshake
+  */
+ export interface socketHandshake extends SocketIO.Handshake{
+    email: string;
+    token: string;
+ }
