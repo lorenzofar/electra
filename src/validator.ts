30fad14ab4a1f39bafb91e242d7ec9bd9436ce80
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-25 00:05:55 +0100

    Add validation logic to Validator class
    Added and documented validation workflow.
    Used callback instead of return value to handle asynchronous requests

--- src/validator.ts
+++ src/validator.ts
@@ -1,11 +1,48 @@
+import * as request from "request";
+
 import * as messages from "./models/messages";
+import * as constants from "./constants";
+import CacheManager from "./cacheManager";
 
 class Validator {
-    public static validateHandshake(handshake: messages.socketHandshake): boolean {
-        /* TODO: Check if the data provided by the client are valid 
-            and if its identity is verified
-        */  
-        return true;
+    public static validateHandshake(handshake: messages.socketHandshake, callback: (result: boolean) => void): void {
+        /**
+         * This method checks if a user can connect to the server or not.
+         * It checks:
+         *      - whther the email is valid (RegEx)
+         *      - whether the email is already connected
+         *      - if the password is not empty
+         *      - If the credentials are correct
+         * If all the above tests are succesfull, the method returns true,
+         * if not or if an operation times out (standard http timeout), returns false
+         */
+
+        if (!handshake) callback(false); // If data are null, stop
+
+        let email = handshake.email; // Get provided email
+        let tempCheckHolder = email && constants.MAILADDR_REGEX.test(email); // Test email against regex
+        if (!tempCheckHolder) callback(false); // If the provided email is not valid, stop
+
+        // Check if the email is already connected
+
+        tempCheckHolder = CacheManager.isUserPresent(email); // Check if the client associated to the email is already connected
+        if (tempCheckHolder) callback(false); // The user is already connected
+
+        // try to login 
+        //TODO: APIs are still to be defined, change with definitive versionregexResult
+        request
+            .get(`${constants.BASE_URL}/login`, (err, response, body) => {
+                if (err) { // An error occurred performing the request
+                    console.log(err);
+                    callback(false);
+                }
+                else { // All's well that ends well
+                    console.log(response.statusCode);
+                    console.log(body);
+                    callback(true);
+                };
+            });
+        callback(true);
     }
 }
 
