94ad34d8ac9c6c6c2ac7b15baf731c9d208179f0
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-26 17:31:49 +0100

    Fix user interface

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,3 @@
 export default interface User {
     email: string;
-    name: string;
-    surname: string;
-    gender?: "male" | "female";
-    profile_image?: string;
-    rank?: number;
-    role?: "standard" | "admin" | "trainer" | "nutritionist" | "psychologist";
-    birthday?: Date;
 }
