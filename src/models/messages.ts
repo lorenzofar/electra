9b1af443baf488f7d73ebca20d4bd744bbd9ec45
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-27 11:21:12 +0100

    Add mode property to handshake interface
    Added a property in the handshake to determine the role of a client
    (data provider or listener)

--- src/models/messages.ts
+++ src/models/messages.ts
@@ -6,8 +6,10 @@
  /**
   * This interface defines the format of messages exchanged during socket handshake
   * It extends the base class of SocketIO.Handshake
+  * mode property is used to determine whether a client is providing or listening to data
   */
  export interface socketHandshake extends SocketIO.Handshake{
     email: string;
     password: string;
+    mode: "provider" | "listener";
  }
