5be2fbb2792d03bb774d3560240049b599236d9c
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-27 16:24:13 +0100

    Add ascii-art on system boot
    Added Electra logo and dolphin

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 /* ===== EXTERNAL MODULES ===== */
 require("dotenv").config();
 import * as http from "http";
+import * as figlet from "figlet";
 
 /* ===== CUSTOM MODULES ===== */
 import Emitter from "./emitter";
 
 const { PORT = 3000 } = process.env; // Get custom port oof fall back to 3000
+
+const dolphin =
+`                                                          __
+                              .__                         / |
+                             /  /                         |  \
+                            /   |                     _-------'_
+                       ____/     \_________      __--"      _/  \_
+         _______------"                    "----"          _-\___/
+     _--"                                               _-"
+ ___<___                                          ___--"
+(-------0                                   __---"
+ '--___                                    /
+       "--___\                _______-----"
+             \\    (____-----"
+              \\    \_
+               '.'..__\ `
 
 /* ===== SERVER INITIALIZATION ===== */
 const server = http.createServer((req, res) => {
@@ -13,6 +30,8 @@ const server = http.createServer((req, res) => {
     res.end("server up");
 })
 server.listen(PORT, () => {
+    console.log(figlet.textSync('ELECTRA'));
+    console.log(dolphin);
     console.log(`> server listening on port ${PORT}`)
 });
 
