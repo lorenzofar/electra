475f01cb5b2bc1536a932eb9751c9452159f51ca
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-28 15:22:38 +0100

    Add return statements on failed validation

--- src/validator.ts
+++ src/validator.ts
@@ -17,22 +17,32 @@ class Validator {
          * if not or if an operation times out (standard http timeout), returns false
          */
 
-        if (!handshake) callback(false); // If data are null, stop
+        if (!handshake) {
+            callback(false); // If data are null, stop
+            return;
+        }
 
         let email = handshake.email; // Get provided email
         let tempCheckHolder = email && constants.MAILADDR_REGEX.test(email); // Test email against regex
-        if (!tempCheckHolder) callback(false); // If the provided email is not valid, stop
+        if (!tempCheckHolder) {
+            callback(false); // If the provided email is not valid, stop
+            return;
+        }
 
         // Check if the email is already connected
 
         tempCheckHolder = CacheManager.isUserPresent(email); // Check if the client associated to the email is already connected
-        if (tempCheckHolder) callback(false); // The user is already connected
+        if (tempCheckHolder) {
+            callback(false); // The user is already connected
+            return;
+        }
 
         //FIXME: Use temporary override until APIs are available
         callback(true);
 
         // try to login 
         /*
+        //FIXME: Use Basic authentication when submitting request
         request
             .get(`${constants.BASE_URL}/login`, (err, response, body) => {
                 if (err) { // An error occurred performing the request
