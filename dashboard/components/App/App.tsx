59d8c8983edc34b5d7754ca5e182d26d26b732e5
Lorenzo Farinelli <lorenzo@Lorenzos-MacBook-Pro.local>
2018-12-08 00:42:45 +0100

    Add react and webpack build chain
    Added packages, configs and refactored frontend to use typescript

---
+++ dashboard/components/App/App.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import "./style.css";
+
+class App extends React.Component{
+    render(){
+        return(
+        <div id="app">
+            <div id="header"></div>
+            <div id="main-container"></div>
+            <div id="status-bar"></div>
+        </div>
+        )
+    };
+}
+
+export default App;
---
+++ dashboard/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import {render} from "react-dom";
+
+import App from "./components/App/App";
+
+render(
+    <App></App>,
+    document.getElementById("root")
+);
