45c7683cc6711ba6ecdf2e00c5c80a00357cc0d8
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-09 22:51:28 +0100

    Add UsersList and StatusBar components and connection handler in App

--- dashboard/components/App/App.tsx
+++ dashboard/components/App/App.tsx
@@ -2,22 +2,31 @@ import * as React from "react";
 
 import SocketManager from "../../providers/socketManager";
 
+/* ===== LOAD COMPONENTS ===== */
+import { UsersList } from "../UsersList/UsersList";
+import {StatusBar} from "../StatusBar/StatusBar";
+
 import "./style.css";
 
-interface AppState{
+
+
+interface AppState {
     //TODO: Define app state
     loading: boolean;
+    connected: boolean;
 }
 
 class App extends React.Component<{}, AppState> {
 
     private SM: SocketManager;
 
-    constructor(props: any){
+    constructor(props: any) {
         super(props);
         this.state = {
-            loading: true
+            loading: true, 
+            connected: false
         }
+        this.handleConnectedStatus = this.handleConnectedStatus.bind(this);
     }
 
     //TODO: How to determine and handle page loading: 
@@ -33,21 +42,33 @@ class App extends React.Component<{}, AppState> {
 
         fetch("/credentials").then(res => {
             return res.json().then(data => {
-                    console.log(data);
-                    this.SM = new SocketManager(data.username, data.password);
-                    console.log("I created the socket manager");
-                    console.log(this.SM);
-                // TODO: Call socket manager providing the fetched credentials
+                this.SM = new SocketManager(data.username, data.password, this.handleConnectedStatus);
             });
         });
     }
+
+
+    /**
+     * This function handles changes in connection to the socket
+     * It provides the status as a state property (TODO:), which is passed to status bar
+     * in order to show connection status to the user
+     * @param connected 
+     */
+    handleConnectedStatus(connected: boolean){
+        console.log(connected);
+        this.setState({connected: connected});
+    }
+
 
     render() {
         return (
             <div id="app">
                 <div id="header"></div>
-                <div id="main-container"></div>
-                <div id="status-bar"></div>
+                <div id="main-container">
+                    <UsersList/>
+                </div>
+                <StatusBar id="status-bar" connected={this.state.connected}></StatusBar>
+                
             </div>
         )
     };
