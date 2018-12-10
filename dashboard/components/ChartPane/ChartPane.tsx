5d53c00068187cfa7795319b50876a34bf879166
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-10 17:36:44 +0100

    Add chart animations
    Also added id property to reference multiple charts

--- dashboard/components/ChartPane/ChartPane.tsx
+++ dashboard/components/ChartPane/ChartPane.tsx
@@ -4,6 +4,7 @@ import * as V from "victory";
 import "./style.css";
 
 interface chartProps {
+    id: string;
     description: string;
     data: any[];
     y: string;
@@ -12,10 +13,14 @@ interface chartProps {
 export class ChartPane extends React.Component<chartProps, {}>{
     render() {
         return (
-            <div className="chart-pane shadowed">
+            <div id={this.props.id} className="chart-pane shadowed">
                 <span className="pane-title">{this.props.description}</span>
                 <V.VictoryChart>
                     <V.VictoryLine
+                        animate={{
+                            duration: 1000,
+                            onLoad: { duration: 500 }
+                        }}
                         data={this.props.data}
                         y={this.props.y}>
                     </V.VictoryLine>
