fe8fdce8e2ae7012f621df10affaca5ce86da6a9
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-11 11:00:55 +0100

    Add stroke color in chart component

--- dashboard/components/ChartPane/ChartPane.tsx
+++ dashboard/components/ChartPane/ChartPane.tsx
@@ -7,10 +7,15 @@ interface chartData {
     [username: string]: any[];
 }
 
+interface chartStrokes {
+    [username: string]: string;
+}
+
 interface chartProps {
     id: string;
     description: string;
-    data: chartData
+    data: chartData;
+    strokes: chartStrokes;
 }
 
 export class ChartPane extends React.Component<chartProps, {}>{
@@ -24,6 +29,9 @@ export class ChartPane extends React.Component<chartProps, {}>{
                         <V.VictoryLine
                             key={i}
                             data={this.props.data[user]}
+                            style={
+                                { data: { stroke: this.props.strokes[user] } }
+                            }
                         >
                         </V.VictoryLine>
                     )}
