f23b83516d45f694c1dbb68821acbcbe5e09735c
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-11 09:15:11 +0100

    Fix chartpane data bindings

--- dashboard/components/ChartPane/ChartPane.tsx
+++ dashboard/components/ChartPane/ChartPane.tsx
@@ -3,27 +3,30 @@ import * as V from "victory";
 
 import "./style.css";
 
+interface chartData {
+    [username: string]: any[];
+}
+
 interface chartProps {
     id: string;
     description: string;
-    data: any[];
-    y: string;
+    data: chartData
 }
 
 export class ChartPane extends React.Component<chartProps, {}>{
     render() {
+        console.log(this.props.data);
         return (
             <div id={this.props.id} className="chart-pane shadowed">
                 <span className="pane-title">{this.props.description}</span>
                 <V.VictoryChart>
-                    <V.VictoryLine
-                        animate={{
-                            duration: 1000,
-                            onLoad: { duration: 500 }
-                        }}
-                        data={this.props.data}
-                        y={this.props.y}>
-                    </V.VictoryLine>
+                    {Object.keys(this.props.data).map((user, i) =>
+                        <V.VictoryLine
+                            key={i}
+                            data={this.props.data[user]}
+                        >
+                        </V.VictoryLine>
+                    )}
                 </V.VictoryChart>
             </div>
         )
