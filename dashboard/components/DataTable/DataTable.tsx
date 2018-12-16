258c7f9d393812a2cc3f5be59ff04a0549e581a5
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-16 18:09:04 +0100

    Add minmax indicators to real-time table
    The maximum value is colored red and the minimum in blue

--- dashboard/components/DataTable/DataTable.tsx
+++ dashboard/components/DataTable/DataTable.tsx
@@ -11,8 +11,47 @@ interface DataTableProps {
 
 const UNDEF_VALUE = "---";
 
+interface maxmin_entry {
+    user: string;
+    val: number;
+}
+
+interface maxmin {
+    max: maxmin_entry;
+    min: maxmin_entry;
+};
+
+interface maxminMap {
+    [sensor: string]: maxmin;
+}
+
 export class DataTable extends React.Component<DataTableProps, null>{
+
+    // Find the maximum value
+    //
     render() {
+
+        let maxminMap: maxminMap = {};
+
+        Object.keys(this.props.data).map(sensor => {
+            let sensorData = this.props.data[sensor];
+            maxminMap[sensor] = { max: { user: null, val: null }, min: { user: null, val: null } };
+            Object.keys(sensorData).forEach(user => {
+                let userData = sensorData[user];
+                let point = userData[userData.length - 1].y;
+                if (maxminMap[sensor].max.val == null || maxminMap[sensor].max.val < point) {
+                    maxminMap[sensor].max.val = point;
+                    maxminMap[sensor].max.user = user;
+                }
+                if (maxminMap[sensor].min.val == null || maxminMap[sensor].min.val > point) {
+                    maxminMap[sensor].min.val = point;
+                    maxminMap[sensor].min.user = user;
+                }
+            });
+        });
+
+        console.log(maxminMap);
+
         return (
             <div id="data-table" className="shadowed">
                 <table className="table header-fixed">
@@ -30,7 +69,15 @@ export class DataTable extends React.Component<DataTableProps, null>{
                                     {Object.keys(this.props.data).map(sensor => {
                                         if (!(user in this.props.data[sensor])) return <td>{UNDEF_VALUE}</td>
                                         let userData: any[] = this.props.data[sensor][user];
-                                        return <td>{userData[userData.length - 1] && (userData[userData.length - 1].y).toFixed(3) || UNDEF_VALUE}</td>
+                                        return (
+                                            <td
+                                                className={
+                                                    `${maxminMap[sensor].max.user === user ? "max" : ""}
+                                                    ${maxminMap[sensor].min.user === user ? "min" : ""}`
+                                                }>
+                                                {userData[userData.length - 1] && (userData[userData.length - 1].y).toFixed(3) || UNDEF_VALUE}
+                                            </td>
+                                        )
                                     })}
                                 </tr>
                             );
