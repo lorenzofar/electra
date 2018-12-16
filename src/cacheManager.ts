e59f740351f09d3b314badd71bc80b01a00bb3db
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-16 13:31:55 +0100

    Add TTL mechanism to cache
    When a user disconnects from the server, its data are not removed,
    but kept in cache and its status set to offline.
    In the meantime a periodic function checks the cache, decreasing
    the TTL of inactive users and removing those who reached 0.
    If a user connects back before expiring, its TTL is restored and its
    status set to active.

--- src/cacheManager.ts
+++ src/cacheManager.ts
@@ -5,9 +5,9 @@ import Cache from "./models/cache";
 import Client from "./models/client";
 import DataPoint from "./models/dataPoint";
 import Dispatcher from "./dispatcher";
-import Emitter from "./emitter";
 
 const CACHE_LENGTH = 100; // The number of most recent data points to keep
+const TTL_MAX = 10 /* minutes */ * 60 /*seconds*/ * 1000 /* milliseconds*/; // The maximum time to wait for a device to restore its connection
 
 interface parsedCacheEntry {
     username: string;
@@ -20,25 +20,60 @@ class CacheManager {
     public static initialize() {
         this.isUserPresent = this.isUserPresent.bind(this);
         this.handleIncomingDataPoint = this.handleIncomingDataPoint.bind(this);
-    }
+        this.cacheCleaner = this.cacheCleaner.bind(this);
 
+        setInterval(this.cacheCleaner, 1000); // run cache cleaning job every 1 second
+    }
 
     /* ===== CACHE MANAGEMENT ===== */
     public static addEntry(user: Client): boolean {
 
-        if (this.isUserPresent(user.username)) return false;
-        this._cache[user.username] = {
-            user: user,
-            data: []
-        };
-        return true;
+        if (this.isUserPresent(user.username)) {
+            // Check if the user is not active 
+            // This is the case when a user has disconnected but
+            // has not yet been removed from cache
+            if (!this._cache[user.username].active) {
+                // Set the user to active
+                this._cache[user.username].active = true;
+                // Reincrease its TTL
+                this._cache[user.username].TTL = TTL_MAX;
+                console.log(`[CACHE] ${user.username} connnected back`);
+                return true;
+            }
+            else return false;
+        }
+        else {
+            this._cache[user.username] = {
+                user: user,
+                data: [],
+                active: true,
+                TTL: TTL_MAX
+            };
+            return true;
+        }
     }
 
     public static removeEntry(username: string): boolean {
         if (!(this.isUserPresent(username))) return false;
-        delete this._cache[username];
-        console.log(`[CACHE] remove entry for ${username}`);
+        //delete this._cache[username];
+        this._cache[username].active = false; // Set connection status to offline
+        console.log(`[CACHE] set ${username} offline`);
         return true;
+    }
+
+    private static cacheCleaner() {
+        //this method is used to keep the cache clean from users that have been inactive for too much time
+        // First get all the inactive users
+        let inactiveUsers = Object.keys(this._cache).filter(k => !this._cache[k].active);
+        // decrease TTL
+        inactiveUsers.forEach(k => this._cache[k].TTL--);
+        // Get users whose TTL is null
+        inactiveUsers = inactiveUsers.filter(k => this._cache[k].TTL === 0);
+        // Remove them from cache
+        inactiveUsers.forEach(k => {
+            delete this._cache[k];
+            console.log(`[CACHE] removed ${k} from cache`);
+        });
     }
 
     /* ===== DATA PROVIDERS ===== */
@@ -47,7 +82,8 @@ class CacheManager {
     }
 
     public static get users(): Client[] {
-        return Object.keys(this._cache).map(k => this._cache[k].user);
+        // Only return active clients
+        return Object.keys(this._cache).filter(k => this._cache[k].active).map(k => this._cache[k].user);
     }
 
     public static get parsedCache(): parsedCacheEntry[] {
@@ -98,7 +134,7 @@ class CacheManager {
      * @param id ID of user to test
      */
     public static isUserPresent(id: string): boolean {
-        if (id in this._cache) return true;
+        if (id in this._cache && this._cache[id].active) return true;
         else return false;
     }
 
--- src/models/cacheEntry.ts
+++ src/models/cacheEntry.ts
@@ -4,4 +4,6 @@ import DataPoint from "./dataPoint";
 export default interface cacheEntry{
     user: Client; // Socket, admin rights and username
     data: DataPoint[]; // Collection of data points
+    active: boolean; // Store information about the connection status of the device
+    TTL: number;
 }
